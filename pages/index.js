3a8eb20f8f3906c686e59ec31e83bfcea086fc4a
Davis Alexandre <62792974+Davis-Ale@users.noreply.github.com>
2024-10-04 12:02:20 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
-    return  <h1> The World is our oyster!
-
-    </h1>
+  return <h1> The World is our oyster!</h1>;
 }
 
 export default Home;
+
+function teste2() {
+  console.log("teste");
+}
